260b0d57292bf470bc4c73c1bfa1984403dfed82
cht <3237126351@qq.com>
2021-11-09 21:08:13 +0800

    I think I should still need coc-explorer, because I use its lsp

--- lua/key.lua
+++ lua/key.lua
@@ -1,7 +1,8 @@
 local keys = {}
 
 keys.keys =function ()
-  vim.api.nvim_set_keymap("n","<M-f>",":NvimTreeToggle<cr>",{ noremap = false, silent = true })
+  vim.api.nvim_set_keymap("n","<M-r>",":NvimTreeToggle<cr>",{ noremap = false, silent = true })
+  vim.api.nvim_set_keymap("n","<M-f>",":CocCommand explorer<cr>",{ noremap = false, silent = true })
   vim.api.nvim_set_keymap("n","<M-g>",":Flog<cr>",{ noremap = false, silent = true })
   vim.api.nvim_set_keymap("n","<M-w>",":Tagbar<cr>",{ noremap = false, silent = true })
   vim.api.nvim_set_keymap("n","<M-d>","<cmd> lua require('functions/vista').Open()<cr>",{ noremap = false, silent = true })
