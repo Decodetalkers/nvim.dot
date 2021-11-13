981a5b71e2df4fd4639c7542233db9e3e0c1d960
cht <3237126351@qq.com>
2021-11-13 18:17:23 +0800

    move space with tab

--- lua/functions/vista.lua
+++ lua/functions/vista.lua
@@ -1,12 +1,12 @@
 local Vista = { state = false }
 Vista.Open = function ()
-   if (Vista.state)
-   then
-       Vista.state = true
-       vim.cmd [[Vista]]
-   else
-	Vista.state = false
-	vim.cmd [[Vista!!]]
-   end
+	if (Vista.state)
+	then
+			Vista.state = true
+			vim.cmd [[Vista]]
+	else
+		Vista.state = false
+		vim.cmd [[Vista!!]]
+	end
 end
 return Vista
