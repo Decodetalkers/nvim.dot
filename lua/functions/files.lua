265da98f0f0b68633153ca59f3886d0f5cefb59f
cht <3237126351@qq.com>
2021-11-14 22:18:31 +0800

    set rust to 4 space

--- lua/functions/files.lua
+++ lua/functions/files.lua
@@ -1,8 +1,16 @@
 local File = {}
 File.Settab = function ()
-	vim.cmd [[set ts=2 ]]
-	vim.cmd [[set tabstop=2 ]]
-	vim.cmd [[set shiftwidth=2 ]]
-	vim.cmd [[set softtabstop=2 ]]
+	if(vim.filetype=="rust")
+	then
+		vim.cmd [[set ts=4 ]]
+		vim.cmd [[set tabstop=4 ]]
+		vim.cmd [[set shiftwidth=4 ]]
+		vim.cmd [[set softtabstop=4 ]]
+	else
+		vim.cmd [[set ts=2 ]]
+		vim.cmd [[set tabstop=2 ]]
+		vim.cmd [[set shiftwidth=2 ]]
+		vim.cmd [[set softtabstop=2 ]]
+	end
 end
 return File
