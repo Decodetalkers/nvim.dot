a2560ea6283e1a1b5e1fac5c428aeaf74d1f1590
cht <3237126351@qq.com>
2021-11-26 12:31:48 +0800

    move requires to plug

--- lua/plug.lua
+++ lua/plug.lua
@@ -58,13 +58,33 @@ plug.loadplug = function ()
 		use {'rbong/vim-flog'}
 		use {'kyazdani42/nvim-web-devicons'} --icons for Nvim tree
 		--use {'JuliaEditorSupport/julia-vim'} --julia-hightlight
-		use {'kyazdani42/nvim-tree.lua'}-- Packer can manage itself
+		use {'kyazdani42/nvim-tree.lua',
+			requires = {
+				require('nvim-tree').setup()
+			}
+		}-- Packer can manage itself
 		--use {'udalov/kotlin-vim'} --kotlin hightlight
 		use {'rust-lang/rust.vim'}
 		use {'Xuyuanp/scrollbar.nvim'}
 		use {
       'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+      run = ':TSUpdate',
+			requires = {
+				require('nvim-treesitter.configs').setup {
+				  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+				  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+				  ignore_install = {}, -- List of parsers to ignore installing
+				  highlight = {
+				    enable = true,              -- false will disable the whole extension
+				    disable = { "c", "rust" },  -- list of language that will be disabled
+				    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+				    -- Instead of true it can also be a list of languages
+				    additional_vim_regex_highlighting = false,
+				  },
+				}
+			}
     }
 
 	end)
