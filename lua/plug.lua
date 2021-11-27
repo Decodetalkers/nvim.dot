6a60b0818b5bdf99ec3db930a43707064203d041
cht <3237126351@qq.com>
2021-11-27 08:45:54 +0800

    remove blackspace

--- lua/plug.lua
+++ lua/plug.lua
@@ -6,10 +6,10 @@ plug.loadplug = function ()
 			'lewis6991/gitsigns.nvim',
 			requires = {
 				'nvim-lua/plenary.nvim'
-				},
-				config = function()
+			},
+			config = function()
 				require('gitsigns').setup()
-				end
+			end
 		}
 		use {'neoclide/coc.nvim', branch = "release"}
 		use {'hrsh7th/nvim-compe'}
@@ -64,18 +64,18 @@ plug.loadplug = function ()
       run = ':TSUpdate',
 			requires = {
 				require('nvim-treesitter.configs').setup {
-				  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-				  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-				  ignore_install = {}, -- List of parsers to ignore installing
-				  highlight = {
-				    enable = true,              -- false will disable the whole extension
-				    disable = { "c", "rust" },  -- list of language that will be disabled
-				    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-				    -- Instead of true it can also be a list of languages
-				    additional_vim_regex_highlighting = false,
-				  },
+				ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+				sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+				ignore_install = {}, -- List of parsers to ignore installing
+				highlight = {
+				  enable = true,              -- false will disable the whole extension
+				  disable = { "c", "rust" },  -- list of language that will be disabled
+				  -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+				  -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+				  -- Using this option may slow down your editor, and you may see some duplicate highlights.
+				  -- Instead of true it can also be a list of languages
+				  additional_vim_regex_highlighting = false,
+				},
 				}
 			}
     }
