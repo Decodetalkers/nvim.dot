d6c69cb4fd35bc4b4b8552a42830a2b0f69ee022
cht <3237126351@qq.com>
2021-11-27 09:32:29 +0800

    update

--- format.sh
+++ format.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-stylua init.lua
-stylua lua/*.lua
-stylua lua/functions/*.lua
+
+format () {
+	if [ "$1" == "--format" ]
+	then
+		stylua init.lua
+		stylua lua/*.lua
+		stylua lua/functions/*.lua
+	elif [ "$1" == "--check" ]; then
+		luacheck init.lua
+		luacheck lua/*
+	else 
+		echo "--format       format the files"
+		echo "--check        check the files"
+	fi
+}
+format $1
