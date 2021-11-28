c9077e5d8c795276f448c543d63ff23cf05872e9
cht <3237126351@qq.com>
2021-11-29 00:09:55 +0800

    use omnisharp, not use csharp_ls temply

--- lua/cmps.lua
+++ lua/cmps.lua
@@ -43,7 +43,7 @@ M.loadlsp = function()
 	end
 
 	local nvim_lsp = require("lspconfig")
-	local servers_lsp = { "julials", "csharp_ls" }
+	local servers_lsp = { "julials" }
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -63,7 +63,7 @@ M.loadlsp = function()
 		--"pyright",
 		"pylsp",
 		"tsserver",
-		-- "omnisharp",
+		"omnisharp",
 		"fsautocomplete",
 		"hls",
 		"texlab",
