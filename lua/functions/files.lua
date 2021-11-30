f6d9ff69b2302bfcc1768451986eb6fed2b1d9b2
cht <3237126351@qq.com>
2021-11-30 21:35:27 +0800

    set the true filetype

--- lua/functions/files.lua
+++ lua/functions/files.lua
@@ -1,11 +1,13 @@
 local File = {}
 File.Settab = function()
-    if vim.filetype == "rust" then
+		print("ss")
+    if (vim.bo.filetype == "rust" or vim.bo.filetype == "cs") then
         vim.cmd([[set ts=4 ]])
         vim.cmd([[set tabstop=4 ]])
         vim.cmd([[set shiftwidth=4 ]])
         vim.cmd([[set softtabstop=4 ]])
     else
+				print("ssf")
         vim.cmd([[set ts=2 ]])
         vim.cmd([[set tabstop=2 ]])
         vim.cmd([[set shiftwidth=2 ]])
