559dd54621418c8c174230696be3997e107f5a1a
cht <3237126351@qq.com>
2021-12-02 01:08:53 +0800

    to italic

--- init.lua
+++ init.lua
@@ -46,3 +46,5 @@ vim.cmd([[packadd! vimspector]])
 vim.cmd(
     [[autocmd BufEnter,BufWinEnter,TabEnter *.rs :lua require'lsp_extensions'.inlay_hints{enabled = {"TypeHint", "ChainingHint", "ParameterHint"}}]]
 )
+vim.cmd([[highlight Function cterm=italic gui=italic]])
+vim.cmd([[highlight Comment cterm=italic gui=italic]])
