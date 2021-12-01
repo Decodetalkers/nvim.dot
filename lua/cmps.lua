62423c082f0422b8c77ceada20bc27c5c3c1a694
cht <3237126351@qq.com>
2021-12-02 01:22:59 +0800

    change the signs

--- lua/cmps.lua
+++ lua/cmps.lua
@@ -192,3 +192,8 @@ cmp.setup({
         { name = "path" },
     },
 })
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
