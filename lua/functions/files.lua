d0b73d5852444f3088282349f6778f9a0d91e8b3
cht <3237126351@qq.com>
2021-12-03 23:49:02 +0800

    reove some vim.cmd

--- lua/functions/files.lua
+++ lua/functions/files.lua
@@ -1,20 +1,25 @@
 local File = {
     ["rust"] = true,
+    ["java"] = true,
     ["cs"] = true,
     ["lua"] = true,
     ["kotlin"] = true,
 }
 File.Settab = function()
     if File[vim.o.filetype] ~= nil then
-        vim.cmd([[set ts=4 ]])
-        vim.cmd([[set tabstop=4 ]])
-        vim.cmd([[set shiftwidth=4 ]])
-        vim.cmd([[set softtabstop=4 ]])
+		vim.opt.tabstop = 4
+		vim.opt.shiftwidth = 4
+		vim.opt.softtabstop = 4
+        --vim.cmd([[set tabstop=4 ]])
+        --vim.cmd([[set shiftwidth=4 ]])
+        --vim.cmd([[set softtabstop=4 ]])
     else
-        vim.cmd([[set ts=2 ]])
-        vim.cmd([[set tabstop=2 ]])
-        vim.cmd([[set shiftwidth=2 ]])
-        vim.cmd([[set softtabstop=2 ]])
+		vim.opt.tabstop = 2
+		vim.opt.shiftwidth = 2
+		vim.opt.softtabstop = 2
+        --vim.cmd([[set tabstop=2 ]])
+        --vim.cmd([[set shiftwidth=2 ]])
+        --vim.cmd([[set softtabstop=2 ]])
     end
 end
 return File
