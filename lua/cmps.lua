76697e89f135b6675591dd577987507b6c28cc28
cht <3237126351@qq.com>
2021-12-22 20:35:21 +0800

    fmt

--- lua/cmps.lua
+++ lua/cmps.lua
@@ -150,16 +150,16 @@ end)
 vim.o.completeopt = "menuone,noselect"
 
 -- luasnip
-local luasnip = require('luasnip')
-require('luasnip.loaders.from_vscode').lazy_load()
+local luasnip = require("luasnip")
+require("luasnip.loaders.from_vscode").lazy_load()
 local has_words_before = function()
-   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
- end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 -- nvim-cmp setup
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
+    return
 end
 local kind_icons = {
     Text = "",
--- lua/plug.lua
+++ lua/plug.lua
@@ -66,9 +66,9 @@ require("packer").startup(function(use)
     use("hrsh7th/nvim-cmp") -- Autocompletion plugin
     use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
     use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
-	use("rafamadriz/friendly-snippets")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-buffer")
+    use("rafamadriz/friendly-snippets")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-buffer")
     use("L3MON4D3/LuaSnip") -- Snippets plugin
     use("williamboman/nvim-lsp-installer")
     use({ "tami5/lspsaga.nvim" })
