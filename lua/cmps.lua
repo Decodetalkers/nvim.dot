731d30ca874a15b0b2f2e484e434a5dbf6e0ebb4
cht <3237126351@qq.com>
2021-12-22 20:34:54 +0800

    add path and lua

--- lua/cmps.lua
+++ lua/cmps.lua
@@ -1,10 +1,3 @@
---	local nvim_lsp = require("lspconfig")
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
---require'lspconfig'.csharp_ls.setup{}
---require'lspconfig'.fsautocomplete.setup{}
-
 local on_attach = function(_, bufnr)
     local function buf_set_keymap(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
@@ -156,14 +149,45 @@ end)
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
 
--- luasnip setup
---local luasnip = require("luasnip")
-
----- nvim-cmp setup
-local luasnip = require("luasnip")
-
+-- luasnip
+local luasnip = require('luasnip')
+require('luasnip.loaders.from_vscode').lazy_load()
+local has_words_before = function()
+   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+ end
 -- nvim-cmp setup
-local cmp = require("cmp")
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+local kind_icons = {
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
+}
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -186,11 +210,13 @@ cmp.setup({
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
         end,
-        ["<S-Tab>"] = function(fallback)
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -198,11 +224,27 @@ cmp.setup({
             else
                 fallback()
             end
+        end, { "i", "s" }),
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snippet]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+            })[entry.source.name]
+            return vim_item
         end,
     },
     sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
+        { name = "buffer" },
         { name = "path" },
     },
 })
