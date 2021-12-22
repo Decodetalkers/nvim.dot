e1e13e46a7236327e580e7c3a9e7dd51a6d47bd3
cht <3237126351@qq.com>
2021-12-22 22:03:12 +0800

    remove useless line

--- lua/plug.lua
+++ lua/plug.lua
@@ -9,22 +9,14 @@ require("packer").startup(function(use)
             require("gitsigns").setup()
         end,
     })
-    --use({ "neoclide/coc.nvim", branch = "release" })
-    --use({ "hrsh7th/nvim-compe" })
     use({ "tpope/vim-fugitive" })
     use({ "chrisbra/csv.vim" })
     use({ "roxma/nvim-yarp" })
     use({ "roxma/vim-hug-neovim-rpc" })
-    --use({ "sirver/UltiSnips" })
-    --use({ "ncm2/ncm2-ultisnips" })
-    --use({ "godlygeek/tabular" })
     use({ "liuchengxu/vista.vim" })
     use({ "preservim/tagbar" })
-    --use({ "skywind3000/asyncrun.vim" })
     use({ "humiaozuzu/dot-vimrc" })
     use({ "ctrlpvim/ctrlp.vim" })
-    --use({ "vim-airline/vim-airline" })
-    --use({ "vim-airline/vim-airline-themes" })
     use({
         "nvim-lualine/lualine.nvim",
         requires = { "kyazdani42/nvim-web-devicons", opt = true },
@@ -33,7 +25,6 @@ require("packer").startup(function(use)
     use({ "natebosch/vim-lsc" })
     use({ "ryanoasis/vim-devicons" })
     use({ "mattn/emmet-vim" })
-    --use({ "honza/vim-snippets" })
     use({ "junegunn/fzf" })
     use({
         "iamcco/markdown-preview.nvim",
@@ -59,8 +50,6 @@ require("packer").startup(function(use)
     use({ "kyazdani42/nvim-tree.lua", requires = {
         require("nvim-tree").setup(),
     } }) -- Packer can manage itself
-    --use {'udalov/kotlin-vim'} --kotlin hightlight
-    --use({ "rust-lang/rust.vim" })
     use({ "Xuyuanp/scrollbar.nvim" })
     use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
     use("hrsh7th/nvim-cmp") -- Autocompletion plugin
@@ -80,7 +69,6 @@ require("packer").startup(function(use)
         "nvim-telescope/telescope.nvim",
         requires = "nvim-lua/plenary.nvim",
     })
-    --use("nvim-lua/lsp_extensions.nvim")
     use({ "akinsho/flutter-tools.nvim", requires = "nvim-lua/plenary.nvim" })
     use("aklt/plantuml-syntax")
     use("tyru/open-browser.vim")
@@ -97,10 +85,6 @@ require("packer").startup(function(use)
                 highlight = {
                     enable = true, -- false will disable the whole extension
                     -- disable = { "markdown" }, -- list of language that will be disabled
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
                     additional_vim_regex_highlighting = false,
                 },
             }),
