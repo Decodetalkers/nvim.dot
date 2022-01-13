48b4156abe132fedf82d2975487c940e2e613210
cht <3237126351@qq.com>
2022-01-13 16:44:09 +0800

    use annother toggle

--- lua/functions/vista.lua
+++ lua/functions/vista.lua
@@ -2,10 +2,10 @@ local Vista = { state = false }
 Vista.Open = function()
     if not Vista.state then
         Vista.state = true
-        vim.cmd([[Vista nvim_lsp ]])
+        vim.cmd([[SymbolsOutlineOpen ]])
     else
         Vista.state = false
-        vim.cmd([[Vista!]])
+        vim.cmd([[SymbolsOutlineClose ]])
     end
 end
 return Vista
