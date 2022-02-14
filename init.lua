154adbfb390e6184ecf020a2dda7d2a96bfb9718
cht <3237126351@qq.com>
2022-02-14 20:47:26 +0800

    move to opt

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ opt.mouse = "a"
 --vim.cmd([[filetype off]])
 local fn = vim.fn
 local packer_bootstrap = {}
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({
         "git",
