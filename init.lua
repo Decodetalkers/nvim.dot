b7c234ed9883cbd099cff35800a2f2ed81c4c445
cht <3237126351@qq.com>
2022-02-14 20:46:08 +0800

    auto install packer.nvim

--- init.lua
+++ init.lua
@@ -4,27 +4,42 @@ opt.wrap = true
 opt.mouse = "a"
 --opt.guifont = "DroidSansMono_Nerd_Font:h11"
 --vim.cmd([[filetype off]])
-vim.cmd([[packadd packer.nvim]])
---opt.foldmethod = "syntax"
-
-local prequire = require("prequire")
-require("plug")
-
-local nvim_lsp = prequire("lspconfig")
-if nvim_lsp then
-    require("key")
-    require("cmps")
-    require("mydap")
+local fn = vim.fn
+local packer_bootstrap = {}
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+    packer_bootstrap = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
 end
-vim.g.foldmethod = "expr"
-vim.g.foldexpr = "nvim_treesitter#foldexpr()"
-opt.termguicolors = true
-opt.background = "dark"
-opt.cursorline = true
-opt.relativenumber = true
-opt.undofile = true
-opt.inccommand = "nosplit"
-opt.undodir = os.getenv("HOME") .. "/.vim/undo"
-if nvim_lsp then
-    require("cmd")
+if packer_bootstrap then
+    vim.cmd([[packadd packer.nvim]])
+    --opt.foldmethod = "syntax"
+
+    local prequire = require("prequire")
+    require("plug")
+
+    local nvim_lsp = prequire("lspconfig")
+    if nvim_lsp then
+        require("key")
+        require("cmps")
+        require("mydap")
+    end
+    vim.g.foldmethod = "expr"
+    vim.g.foldexpr = "nvim_treesitter#foldexpr()"
+    opt.termguicolors = true
+    opt.background = "dark"
+    opt.cursorline = true
+    opt.relativenumber = true
+    opt.undofile = true
+    opt.inccommand = "nosplit"
+    opt.undodir = os.getenv("HOME") .. "/.vim/undo"
+    if nvim_lsp then
+        require("cmd")
+    end
 end
