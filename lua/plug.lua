f52503693d56620c0534983414221ae90fa64f99
cht <3237126351@qq.com>
2022-02-21 18:44:33 +0800

    fix

--- lua/plug.lua
+++ lua/plug.lua
@@ -194,6 +194,14 @@ require("packer").startup(function(use)
                     -- disable = { "markdown" }, -- list of language that will be disabled
                     additional_vim_regex_highlighting = false,
                 },
+                rainbow = {
+                    enable = true,
+                    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+                    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                    -- colors = {}, -- table of hex strings
+                    -- termcolors = {} -- table of colour name strings
+                },
             })
         end,
     })
