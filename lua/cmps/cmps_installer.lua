90a8a839ba159c05e51aee58504fa7d9e7d6afda
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-05-11 16:50:39 +0800

    update installer method

--- lua/cmps/cmps_installer.lua
+++ lua/cmps/cmps_installer.lua
@@ -1,5 +1,3 @@
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.setup({})
 local servers = {
     "html",
     "cssls",
@@ -38,14 +36,6 @@ local servers = {
     --"denols",
     "taplo",
 }
-
-for _, name in pairs(servers) do
-    local ok, server = lsp_installer.get_server(name)
-    -- Check that the server is supported in nvim-lsp-installer
-    if ok then
-        if not server:is_installed() then
-            vim.notify("Installing " .. name)
-            server:install()
-        end
-    end
-end
+require("nvim-lsp-installer").setup({
+    ensure_installed = servers,
+})
