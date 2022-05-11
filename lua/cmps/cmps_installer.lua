68760740a5fe86affc5f8b31b9f9a6c341636006
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-05-11 16:42:48 +0800

    use notify

--- lua/cmps/cmps_installer.lua
+++ lua/cmps/cmps_installer.lua
@@ -44,7 +44,7 @@ for _, name in pairs(servers) do
     -- Check that the server is supported in nvim-lsp-installer
     if ok then
         if not server:is_installed() then
-            print("Installing " .. name)
+            vim.notify("Installing " .. name)
             server:install()
         end
     end
